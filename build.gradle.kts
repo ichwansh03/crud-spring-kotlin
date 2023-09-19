aa996d931919aadada29787d4193e16fbb294856
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-19 20:53:49 +0700

    chore: change mysql runtime library

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("mysql:mysql-connector-java:8.0.32")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
