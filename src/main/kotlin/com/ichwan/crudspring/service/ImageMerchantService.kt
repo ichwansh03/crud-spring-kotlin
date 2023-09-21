e7d00ea833f032c2f0e82f4a7cdb90ddb8843d4c
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-21 13:19:26 +0700

    chore: add download file service

--- src/main/kotlin/com/ichwan/crudspring/service/ImageMerchantService.kt
+++ src/main/kotlin/com/ichwan/crudspring/service/ImageMerchantService.kt
@@ -1,23 +1,36 @@
 package com.ichwan.crudspring.service
 
 import com.ichwan.crudspring.model.ImageMerchant
+import com.ichwan.crudspring.model.response.ImageResponse
 import com.ichwan.crudspring.repository.ImageMerchantRepository
 import com.ichwan.crudspring.service.datasource.ImageMerchantDataSource
 import com.ichwan.crudspring.util.ImageUtil
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import java.util.Optional
 
 @Service
 class ImageMerchantService (private val imageMerchantRepository: ImageMerchantRepository) : ImageMerchantDataSource {
-    override fun upload(file: MultipartFile) {
+    override fun upload(file: MultipartFile) : ImageResponse {
         val imageData = ImageMerchant()
 
-        imageData.image_name = file.originalFilename
-        imageData.image_type = file.contentType
-        imageData.image_outlet = ImageUtil.compressImage(file.bytes)
+        imageData.imageName = file.originalFilename
+        imageData.imageType = file.contentType
+        imageData.imageOutlet = ImageUtil.compressImage(file.bytes)
 
         imageMerchantRepository.save(imageData)
+
+        return ImageResponse(
+            name = file.originalFilename,
+            message = "Image successfully uploaded"
+        )
     }
 
+    override fun download(fileName: String): ByteArray {
+
+        val imagerepo: Optional<ImageMerchant> = imageMerchantRepository.findByImageName(fileName)
+
+        return ImageUtil.decompressImage(imagerepo.get().imageOutlet)
+    }
 
 }
--- src/main/kotlin/com/ichwan/crudspring/service/datasource/ImageMerchantDataSource.kt
+++ src/main/kotlin/com/ichwan/crudspring/service/datasource/ImageMerchantDataSource.kt
@@ -1,8 +1,11 @@
 package com.ichwan.crudspring.service.datasource
 
+import com.ichwan.crudspring.model.response.ImageResponse
 import org.springframework.web.multipart.MultipartFile
 
 interface ImageMerchantDataSource {
 
-    fun upload(file: MultipartFile)
+    fun upload(file: MultipartFile) : ImageResponse
+
+    fun download(fileName: String): ByteArray
 }
