9822b6838383b6db944c189011ad2af0b92e684a
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-21 13:20:01 +0700

    chore: add method `findByImageName`

--- src/main/kotlin/com/ichwan/crudspring/repository/ImageMerchantRepository.kt
+++ src/main/kotlin/com/ichwan/crudspring/repository/ImageMerchantRepository.kt
@@ -3,6 +3,10 @@ package com.ichwan.crudspring.repository
 import com.ichwan.crudspring.model.ImageMerchant
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-interface ImageMerchantRepository : CrudRepository<ImageMerchant, Int>
+interface ImageMerchantRepository : CrudRepository<ImageMerchant, Int>{
+
+    fun findByImageName(imageName: String?): Optional<ImageMerchant>
+}
